a6e1e2103f15c1fc1ba1a3ecd14dc173ea68d14c
Novolodskiy <novolodskiyda@gmail.com>
2025-04-27 00:07:11 +0300

    HW for lesson 20

---
+++ homework/dima_novolodskiy/hw19/conftest.py
@@ -0,0 +1,36 @@
+import pytest
+import requests
+from faker import Faker
+
+fake = Faker()
+
+@pytest.fixture()
+def object_for_test():
+    body = {
+        "name": f"{fake.name()}",
+        "data": {"color": f"{fake.color()}", "country": f"{fake.country()}"}
+    }
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(
+        'http://167.172.172.115:52353/object',
+        json=body,
+        headers=headers
+    )
+    object_id = response.json()['id']
+    yield object_id
+    requests.delete(f'http://167.172.172.115:52353/object/{object_id}')
+
+
+@pytest.fixture(scope="session")
+def session_scope():
+    print("Start testing")
+    yield
+    print("Testing completed")
+
+
+# Function-scoped fixture for before/after each test
+@pytest.fixture()
+def function_scope():
+    print("before test")
+    yield
+    print("after test")
---
+++ homework/dima_novolodskiy/hw19/test_get/conftest.py
@@ -0,0 +1,22 @@
+import requests
+from faker import Faker
+import pytest
+
+fake = Faker()
+
+
+@pytest.fixture()
+def object_for_test():
+    body = {
+        "name": f"{fake.name()}",
+        "data": {"color": f"{fake.color()}", "country": f"{fake.country()}"}
+    }
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(
+        'http://167.172.172.115:52353/object',
+        json=body,
+        headers=headers
+    )
+    object_id = response.json()['id']
+    yield object_id
+    requests.delete(f'http://167.172.172.115:52353/object/{object_id}')
