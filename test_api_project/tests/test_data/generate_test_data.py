79c186db5cf167c4bd73d9b7d030a975be2a94d8
Novolodskiy <novolodskiyda@gmail.com>
2025-05-03 18:51:08 +0300

    fixed flake8

--- test_api_project/tests/test_data/generate_test_data.py
+++ test_api_project/tests/test_data/generate_test_data.py
@@ -4,15 +4,10 @@ fake = Faker()
 
 
 def generate_test_data():
-    """Generate test data for API testing.
-    
-    Returns:
-        dict: Dictionary containing test data with name, color and country.
-    """
     return {
         "name": fake.name(),
         "data": {
             "color": fake.color(),
             "country": fake.country()
         }
-    } 
+    }
